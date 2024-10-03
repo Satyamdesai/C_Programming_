f28c0f8782f26ccd0f4ef90a63aaec7f6942a853
Satyamdesai <satyamdesai1012@gmail.com>
2024-10-03 15:14:18 +0530

    Add files via upload

---
+++ Basic _Programs/Addition.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int No1 =0,No2=0,Sum=0;
+    printf("\n Enter First Number :");
+    scanf("%d",&No1);
+    printf("\n Enter Second Number :");
+    scanf("%d",&No2);
+    Sum = No1 + No2;
+    printf("\n Sum = %d",Sum);
+    getch();
+    return 0;
+}
