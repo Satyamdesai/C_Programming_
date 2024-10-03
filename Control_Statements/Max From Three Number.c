776e791f808a760228a67da06f15659a35d73e3a
Satyamdesai <satyamdesai1012@gmail.com>
2024-10-03 15:18:36 +0530

    Add files via upload

---
+++ Control_Statements/Max From Three Number.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+int main()
+
+{
+    int Num1 =0,Num2 =0, Num3 =0;
+
+
+    printf("Enter value of Num 1:");
+    scanf("%d",&Num1);
+    printf("Enter value of NUM2:");
+    scanf("%d",&Num2);
+    printf("Enter value of Num3:");
+    scanf("%d",&Num3);
+
+
+    if((Num1>Num2) && (Num1>Num3))
+    {
+    printf("The Num1 is the maximum Number.");
+    }
+    else if((Num2>Num1) && (Num2>Num3))
+    {
+    printf("The Num2 is the maximum Number.");
+    }
+
+    else
+    {
+        printf("The Num3 is the Maximum number.");
+    }
+    getch();
+    return 0;
+
+    }
+
+
